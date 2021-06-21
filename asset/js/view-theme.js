9c4019cd4bcc172100279adbe7fa067ffc7296cf
CVEY <clement.vey@ynov.com>
2021-06-21 17:03:59 +0200

    on attend le poist de guigui

--- asset/js/view-theme.js
+++ asset/js/view-theme.js
@@ -1,5 +1,7 @@
 const arrowTop = document.querySelectorAll(".arrowTop")[0]
 const arrowBottom = document.querySelectorAll(".arrowBottom")[0]
+const arrowLeft = document.querySelectorAll(".arrowLeft")[0]
+const arrowRight = document.querySelectorAll(".arrowRight")[0]
 
 arrowTop.addEventListener("click", () => {
     const theme = document.getElementById("test")
@@ -18,3 +20,23 @@ arrowBottom.addEventListener("click", () => {
         behavior:"smooth"
     })
 })
+
+arrowLeft.addEventListener("click", () => {
+    const theme = document.getElementById("test")
+    theme.scrollTo({
+        top:0,
+        left:theme.scrollLeft-200,
+        behavior:"smooth"
+    })
+})
+
+arrowRight.addEventListener("click", () => {
+    const theme = document.getElementById("test")
+    theme.scrollTo({
+        top:0,
+        left:theme.scrollLeft+200,
+        behavior:"smooth"
+    })
+})
+
+
