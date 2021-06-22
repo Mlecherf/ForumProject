a248a6b35a68d8d133ae391e45996425546cdb7b
Guillaume Petta <guillaume.petta@ynov.com>
2021-06-22 11:58:57 +0200

    like fait

---
+++ asset/js/Post-view.js
@@ -0,0 +1,37 @@
+const LikePost = document.getElementById("Like-Button")
+LikePost.value = "non"
+const likeForm = document.getElementById("Likeform")
+ADD_Like()
+function ADD_Like(){
+    LikePost.addEventListener("click",()=>{
+        if(LikePost.value == "non"){
+            console.log("false to true");
+            LikePost.value = "oui";
+            LikePost.style.color = "#FFCB77"
+        }else if (LikePost.value == "oui"){
+            console.log("true to false");
+            LikePost.value = "non";
+            LikePost.style.color = "#c29958"
+        }
+    })
+}
+likeForm.addEventListener("submit",function(){
+    fetch('/like', {
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            Like: LikePost.value
+        })  
+    })
+    .then(function(response){
+        return response.text
+    })
+    .catch(function(error){
+        console.error(error)
+    })
+})
+
+
+
