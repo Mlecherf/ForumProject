f13ba2f5e7226d624d9cf26cbd95e42e8573e8c3
Guillaume Petta <guillaume.petta@ynov.com>
2021-06-23 12:31:12 +0200

    faire css post mod et relier a la BDD

--- asset/js/Post-view.js
+++ asset/js/Post-view.js
@@ -1,31 +1,113 @@
 const LikePost = document.getElementById("Like-Button")
 LikePost.value = "non"
-const likeForm = document.getElementById("Likeform")
 const PostModif = document.getElementById("modify-post")
-const modif = document.getElementById("modify")
-ADD_Like()
-function ADD_Like(){
-    LikePost.addEventListener("click",()=>{
-        if(LikePost.value == "non"){
-            console.log("false to true");
-            LikePost.value = "oui";
-            LikePost.style.color = "#FFCB77"
-        }else if (LikePost.value == "oui"){
-            console.log("true to false");
-            LikePost.value = "non";
-            LikePost.style.color = "#c29958"
-        }
-    })
-}
-likeForm.addEventListener("submit",function(){
-    fetch('/like', {
+const modif = document.getElementById("pop_post_modify")
+// ADD_Like()
+// function ADD_Like(){
+//     LikePost.addEventListener("click",()=>{
+//         if(LikePost.value == "non"){
+//             console.log("false to true");
+//             LikePost.value = "oui";
+//             LikePost.style.color = "#FFCB77"
+//         }else if (LikePost.value == "oui"){
+//             console.log("true to false");
+//             LikePost.value = "non";
+//             LikePost.style.color = "#c29958"
+//         }
+//         fetch('/like', {
+//             headers : {
+//                 'Accept': 'application/json',
+//                 'Content-Type':'application/json'
+//             },
+//             body:JSON.stringify({
+//                 Like: LikePost.value
+//             })  
+//         })
+//         .then(function(response){
+//             return response.text
+//         })
+//         .catch(function(error){
+//             console.error(error)
+//         })
+//     })
+// }
+const NameMod = document.getElementById("post_name_mod");
+const ContentMod = document.getElementById("post_content_mod");
+
+
+
+PostModif.addEventListener("click",function(){
+    modif.style.display = "block"
+    NameMod.value = document.getElementById("User-Post-Name").innerHTML
+    ContentMod.value = document.getElementById("text").innerHTML
+    document.getElementById("label_name_mod").innerHTML = `Name : character ${NameMod.value.length}/25`
+    document.getElementById("label_content_mod").innerHTML = `Description : character ${ContentMod.value.length}/2000`
+    document.getElementById("post_submit_mod").innerHTML = "Change"
+})
+document.getElementsByClassName("closemod")[0].addEventListener("click",()=>{
+    modif.style.display = "none"
+    document.getElementById("label_name_mod").innerHTML = "Name : character 0/25"
+    NameMod.value = ""
+    NameMod.style.borderColor = "#FFCB77"
+    document.getElementById("label_content_mod").innerHTML = `Description : character 0/2000`
+    ContentMod.value = ""
+    ContentMod.style.borderColor = "#FFCB77"
+    document.getElementById("post_submit_mod").setAttribute("disabled",true)
+})
+
+// --name input management--
+NameMod.addEventListener("input", ()=>{
+    console.log("name")
+    console.log(document.getElementById("label_name_mod").innerHTML)
+    if (NameMod.value.length < 4){
+        document.getElementById("label_name_mod").innerHTML = `Name : not enough character ${NameMod.value.length}/25`
+        NameMod.style.borderColor = "red"
+    }else if(NameMod.value.length > 25){
+        document.getElementById("label_name_mod").innerHTML = `Name : too many character ${NameMod.value.length}/25`
+        NameMod.style.borderColor = "red"
+    }else{
+        document.getElementById("label_name_mod").innerHTML = `Name : character ${NameMod.value.length}/25`
+        NameMod.style.borderColor = "green"
+    }
+    ADD_enabled()
+})
+// content post management
+ContentMod.addEventListener("input", ()=>{
+    console.log("content")
+    console.log(document.getElementById("label_content_mod").innerHTML)
+    if (ContentMod.value.length < 4){
+       document.getElementById("label_content_mod").innerHTML = `Description : not enough character ${ContentMod.value.length}/2000`
+       ContentMod.style.borderColor = "red"
+    }else if(ContentMod.value.length > 2000){
+       document.getElementById("label_content_mod").innerHTML = `Description : too many character ${ContentMod.value.length}/2000`
+       ContentMod.style.borderColor = "red"
+    }else{
+       document.getElementById("label_content_mod").innerHTML = `Description : character ${ContentMod.value.length}/2000`
+       ContentMod.style.borderColor = "green"
+    }
+    ADD_enabled()
+})
+function ADD_enabled (){
+    if ((NameMod.style.borderColor == "green")&&(ContentMod.style.borderColor == "green")){
+        document.getElementById("post_submit_mod").removeAttribute("disabled")
+    }else{
+        document.getElementById("post_submit_mod").setAttribute("disabled",true)
+    }
+} 
+document.getElementById("myFormMod").addEventListener('submit',function (e){
+    modif.style.display = "none"
+    e.preventDefault()
+    fetch('/modifpost', {
+        method: 'post',
         headers : {
             'Accept': 'application/json',
             'Content-Type':'application/json'
         },
         body:JSON.stringify({
-            Like: LikePost.value
-        })  
+            Name: NameMod.value,
+            Content: ContentMod.value,
+            Id_post: document.getElementById("Post_id_modif").value
+        })
     })
     .then(function(response){
         return response.text
@@ -33,9 +115,11 @@ likeForm.addEventListener("submit",function(){
     .catch(function(error){
         console.error(error)
     })
+    document.getElementById("label_name_mod").innerHTML = "Name : character 0/25"
+    NameMod.value = ""
+    NameMod.style.borderColor = "#FFCB77"
+    document.getElementById("label_content_mod").innerHTML = `Description : character 0/2000`
+    ContentMod.value = ""
+    ContentMod.style.borderColor = "#FFCB77"
+    document.getElementById("post_submit_mod").setAttribute("disabled",true)
 })
-PostModif.addEventListener("click",function(){
-    modif.style.display = "block"
-})
-
-
